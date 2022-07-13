301012af76c486569c51acbe1c5b18f9c3bcff93
Bruno Andrade <99356801+DinDja@users.noreply.github.com>
2022-07-13 13:14:44 -0300

    Delete assets directory

--- assets/js/navhidden.js
+++
@@ -1,8 +0,0 @@
-let navhid =  document.querySelector("#navhidden");
-window.onscroll = function (e) {  
-    if(window.scrollY > 50){
-        navhid.style.display = "none";
-    }else{
-        navhid.style.display = "block";
-    }
-} 
